a13dcab4d522d8b05a27e3885c03119a437d6419
tien96ng <tien96ng@gmail.com>
2021-02-12 12:16:39 -0800

    Add fetch api functionality to ExchangeService.js

--- src/js/exchange-service.js
+++ src/js/exchange-service.js
@@ -1,6 +1,7 @@
 export default class ExchangeService {
     static async usdExchangeRate() {
         try {
+            const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`)
             if(!response.ok) {
                 throw Error(response);
             }
