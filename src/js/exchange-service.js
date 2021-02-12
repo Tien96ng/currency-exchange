b7d04f80ee05fda11832543fd6c09ebd6677cb7d
tien96ng <tien96ng@gmail.com>
2021-02-12 12:15:28 -0800

    Create API try and catch for API call.

--- src/js/exchange-service.js
+++ src/js/exchange-service.js
@@ -1,3 +1,12 @@
 export default class ExchangeService {
-    return;
+    static async usdExchangeRate() {
+        try {
+            if(!response.ok) {
+                throw Error(response);
+            }
+            return response.json();
+        } catch(error) {
+            return error
+        }
+    }
 }
