58949557c5f7ae357ca20ef0b783e33f0e14cfa1
tien96ng <tien96ng@gmail.com>
2021-02-12 16:01:35 -0800

    Add error checking to display on HTML and css styling for cursor.

--- src/js/displayCurrency.js
+++ src/js/displayCurrency.js
@@ -1,13 +1,23 @@
 import $ from 'jquery';
 import ExchangeService from "./exchange-service.js";
 
-export default async function displayCurrency() {
+export async function displayCurrency() {
   const response = await ExchangeService.usdExchangeRate();
-  const currencies = new Map();
-  for (const property in response.conversion_rates) {
-    currencies.set(property, response.conversion_rates[property])
+  if(response["error-type"]) {
+    $(".error").text(`Please check your API Endpoint Call: ${response["error-type"]}`);
+  } else {
+    const currencies = new Map();
+    for (const property in response.conversion_rates) {
+      currencies.set(property, response.conversion_rates[property]);
+    }
+    currencies.forEach((value, key) => {
+      $("#to").append(`<option value="${key}">${key}</option>`);
+    });
   }
-  currencies.forEach((value, key) => {
-    $("#to").append(`<option value="${key}">${key}</option>`)
-  });
+}
+
+export async function converter(usd, currency) {
+  const result = await (usd * currency);
+  console.log(result);
+  return result;
 }
--- src/main.js
+++ src/main.js
@@ -2,9 +2,11 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-import displayCurrency from "./js/displayCurrency.js";
+import {displayCurrency, converter} from "./js/displayCurrency.js";
 
 
 $(document).ready(() => {
   displayCurrency();
+  converter(1, 3.6725);
+
 });
